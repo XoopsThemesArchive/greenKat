c6e9c0398b07be65db5b8c5665262d45e5307384
mambax7 <mambax7@gmail.com>
2015-05-05 09:06:55 -0400

    Original files

---
+++ greenKat/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "GreenKat";
+	$txc_themeinfo["author"] = "mypilottalk";
+	$txc_themeinfo["author_url"] = "http://mypilottalk.com";
+	$txc_themeinfo["detail_url"] = "http://mypilottalk.com";
+	$txc_themeinfo["download_url"] = "http://mypilottalk.com";
+?>
